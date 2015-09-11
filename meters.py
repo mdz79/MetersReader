0bd8fef02dc0da3590842a2fb2afa75973994297
MDZ <michal@dzacovsky.com>
2015-09-11 15:32:50 +0200

    First update

---
+++ meters.py
@@ -0,0 +1,29 @@
+'''This is my First attempt to use Raspberry Pi B+ as electromechanical meter reader.
+
+The main challenges:
+1) They are really fast (up to 10 impulses in a second)
+2) The are on 12V board - I need to solve how to connect them and not to burn the RPi
+
+'''
+
+import RPi.GPIO as GPIO            	# importing GPIO 
+GPIO.setmode(GPIO.BOARD)			# setting that we use Board numbering of pins
+GPIO.setup(18, GPIO.IN)				# setting pin 18 as an input
+GPIO.cleanup ()
+loop1 = True
+
+
+
+
+
+'''MAIN PROGRAM LOOP'''
+while loop1 == TRUE:
+	if GPIO.input(18) == True:
+		print "Counting ... "
+		x = 1
+		y = y + X
+		print y
+
+
+
+			
